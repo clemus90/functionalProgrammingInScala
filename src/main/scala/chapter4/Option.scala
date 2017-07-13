bda96e25222550b85ed8366e7ff5d1fb2332e7f5
Christian Lemus <crlg90@gmail.com>
2017-07-13 00:23:31 -0500

    reaches 4.4

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -44,9 +44,11 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = ???
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => Math.pow(x - m, 2))))
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = ???
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a.flatMap((x) => b.map((y) => f(a,b)))
 
   def sequence[A](a: List[Option[A]]): Option[List[A]] = ???
 
