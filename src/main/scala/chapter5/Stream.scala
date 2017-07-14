524b49c70ea4d7bb192e52fb5a903ca7edb04257
Christian Lemus <crlg90@gmail.com>
2017-07-14 00:11:25 -0500

    reaches 5.4

---
+++ src/main/scala/chapter5/Stream.scala
@@ -0,0 +1,79 @@
+package chapter5
+
+import Stream._
+trait Stream[+A] {
+
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(h, t) => h() :: t().toList
+  }
+
+  def toListTailRec: List[A] = {
+    def loop(acc: List[A], curr: Stream[A]):List[A] = curr match {
+      case Empty => acc
+      case Cons(h, t) => loop(h() :: acc, t())
+    }
+    loop(List(), this).reverse
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
+    this match {
+      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case _ => z
+    }
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
+
+  @annotation.tailrec
+  final def find(f: A => Boolean): Option[A] = this match {
+    case Empty => None
+    case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
+  }
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h,_) if (n == 1) => cons(h(), Empty)
+    case Cons(h, t) if (n > 1) => cons(h(), t().take(n-1))
+    case Empty => Empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, _) if (n == 0) => this
+    case Cons(h, t) if (n > 0) => t().drop(n-1)
+    case Empty => Empty
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if (p(h())) => t().takeWhile(p)
+    case _ => this
+  }
+
+  def forAll(p: A => Boolean): Boolean = ???
+
+  def headOption: Option[A] = ???
+
+  // 5.7 map, filter, append, flatmap using foldRight. Part of the exercise is
+  // writing your own function signatures.
+
+  def startsWith[B](s: Stream[B]): Boolean = ???
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = hd
+    lazy val tail = tl
+    Cons(() => head, () => tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = Stream.cons(1, ones)
+  def from(n: Int): Stream[Int] = ???
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = ???
+}
