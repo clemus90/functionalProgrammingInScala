fa29a253b84dd921ff1779bb8f1945eeb6b2e015
Christian Lemus <crlg90@gmail.com>
2017-08-15 21:18:24 -0500

    Solves chapter 6 and 7

--- src/main/scala/chapter6/State.scala
+++ src/main/scala/chapter6/State.scala
@@ -73,20 +73,66 @@ object RNG {
     }
   }
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def ints2(count: Int)(rng: RNG): Rand[List[Int]] = {
+    sequence(List.fill(count)(int))
+  }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rand => {
+      val (a, rand1) = ra(rand)
+      val (b, rand2) = rb(rand1)
+      (f(a,b), rand2)
+    }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight(unit[List[A]](List()))((item, acc) => map2(item, acc)(_ :: _))
+
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (inner, rng2) = f(rng)
+      g(inner)(rng2)
+    }
+
+  def nonNegativeLessThan(n: Int): Rand[Int] =
+    flatMap(nonNegativeInt)(num => {
+      val mod = num % n
+      if(num + (n - 1) - mod >= 0)
+        unit(mod)
+      else
+        nonNegativeLessThan(n)
+    })
+
+  def map1[A,B](s: Rand[A])(f: A => B): Rand[B] = flatMap(s)(x => unit(f(x)))
+
+  def map2_2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a,b)))
 }
 
 case class State[S,+A](run: S => (A, S)) {
   def map[B](f: A => B): State[S, B] =
-    ???
+    flatMap(a => State.unit(f(a)))
   def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+    flatMap(a => sb.map(b => f(a,b)))
+  def flatMap[B](f: A => State[S, B]): State[S, B] = new State(s => {
+    val (item, state) = run(s)
+    f(item).run(state)
+  })
+}
+
+object State{
+  type Rand[A] = State[RNG, A]
+  def unit[S,A](a: A): State[S,A] = State((s:S) => (a, s))
+  def sequence[S,A](fs: List[State[S,A]]): State[S, List[A]] =
+    fs.foldRight(unit[S,List[A]](Nil))((item, acc) => item.map2(acc)(_ :: _))
+
+  def modify[S](f: S => S): State[S, Unit] = for {
+    s <- get // Gets the current state and assigns it to `s`.
+    _ <- set(f(s)) // Sets the new state to `f` applied to `s`.
+  } yield ()
+
+  def get[S]: State[S, S] = State(s => (s, s))
+
+  def set[S](s: S): State[S, Unit] = State(_ => ((), s))
 }
 
 sealed trait Input
@@ -95,7 +141,20 @@ case object Turn extends Input
 
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
-object State {
-  type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
+object Candy {
+  import State._
+
+  def update = (i: Input) => (m: Machine) => (i,m) match {
+    case (_, Machine(_, 0, _)) => m
+    case (Coin, Machine(false, _, _)) => m
+    case (Turn, Machine(true, _, _)) => m
+    case (Coin, Machine(true, ca, co)) => Machine(false, ca, co + 1)
+    case (Turn, Machine(false, ca, co)) => Machine(true, ca - 1, co)
+
+  }
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = for {
+    _ <- sequence(inputs.map(x => modify(update(x))))
+    s <- get
+  } yield(s.candies, s.coins)
 }
